30d5a3ae83563c1d512cbd03840be084b0551434
Abiel Ortega <abielortega90@gmail.com>
2019-09-08 12:48:19 -0500

    added some files to separate the Javasript Code

---
+++ practice.js
@@ -0,0 +1,12 @@
+console.log("Hello there friends");
+
+// Made my own Question Builder here lol its pretty cool in my opinion.
+alert('Lets see if you can drive');
+var age = prompt('What is your age?');
+if (age < 16) {
+  alert('You cant drive!');
+}else if (age == 16) {
+  alert('Yay you can finally drive!!');
+}else {
+ alert('You probably have been driving already so be safe');
+ }
