8d4eb4ac3940c55d97adce712e04d81e05cb6ec0
Abiel Ortega <abielortega90@gmail.com>
2019-09-16 14:13:11 -0500

    added some style and some javascript. I understand how objects work omg yes!! Im learning!!1

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,33 @@
 console.log("Hello there friends");
+
+var car = {
+  make: 'Tesla',
+  type: 'Rover',
+  color: 'Black',
+  isTurnedOn: false,
+  numberOfWheels: 4,
+  seats: [
+    'seat1',
+    'seat2',
+    'seat3',
+    'seat4',
+  ],
+
+  // turnOn: function () {
+  //   this.isTurnedOn = true;
+  //   return 'isTurnedOn Function Changed to '+ this.isTurnedOn;
+  // },
+  // fly: function () {
+  //   alert('Fly');
+  // },
+  
+  switchCar: function (isOn) {
+    console.log ('Turn car '+isOn);
+    
+    if (isOn == true) {
+      this.isTurnedOn = true;
+    } else {
+      this.isTurnedOn = false;
+    }
+  }
+}
